ca140c752ba4342dcb71ffb11dd7f5f4fab51bf8
Neel Smith <dnsmith.neel@gmail.com>
2016-09-02 12:22:37 -0400

    Add initial basic machine.

---
+++ system/bootstrap.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+#
+# Add repository for an early-21st-century version of gradle:
+add-apt-repository ppa:cwchien/gradle
+apt-get update
+
+
+#########################################################
+### Install packages required for HMT editing ###########
+#########################################################
+
+# Clean up any catastrophic reformatting that
+# 'git clone' could introduce on a Windows box:
+apt-get install -y dos2unix
+/usr/bin/dos2unix /vagrant/system/*sh
+/usr/bin/dos2unix /vagrant/system/dotprofile
+/usr/bin/dos2unix /vagrant/scripts/*sh
+
+# and add bomstrip utils in case XML Copy Editor
+# or evil Windows software tries to insert a BOM
+# in your editorial work:
+apt-get install -y bomstrip
+
+# version control
+apt-get install -y git
+
+
+
+# XML editor
+apt-get install -y xmlcopyeditor
+
+#########################################################
+### Web
+apt-get -y install firefox
+## alpheios must be manually installed?
+
+
+#########################################################
+### Markdown bundle
+#########################################################
+
+apt-get -y install mdpress
+apt-get -y install pandoc
+
+
+# Set up vagrant user account:
+cp /vagrant/system/dotprofile /home/vagrant/.profile
+chown vagrant:vagrant /home/vagrant/.profile
+
+cp /vagrant/system/mimeapps.list /home/vagrant/.config
+chown vagrant:vagrant /home/vagrant/.config/mimeapps.list
+
+rm /home/vagrant/.config/plank/dock1/launchers/*.dockitem
+cp /vagrant/system/plank-dock1-launchers/*.dockitem /home/vagrant/.config/plank/dock1/launchers
+chown vagrant:vagrant /home/vagrant/.config/plank/dock1/launchers/*.dockitem
+
+
+if [ ! -e /usr/bin/atom ]
+    then
+      # Add Atom:
+      echo "Downloading .deb file for Atom"
+      /usr/bin/wget https://github.com/atom/atom/releases/download/v1.0.11/atom-amd64.deb 2> /tmp/atom-log.txt
+      echo "Download complete, installing Atom"
+      /usr/bin/dpkg --install atom-amd64.deb
+      echo "Atom installed. "
+fi
+
+apt-get -y -q upgrade
+apt-get -y -q install software-properties-common htop
+add-apt-repository ppa:webupd8team/java
+apt-get -y -q update
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+apt-get -y -q install oracle-java8-installer
+update-java-alternatives -s java-8-oracle
+
+
+# http://downloads.lightbend.com/scala/2.11.7/scala-2.11.7.deb ?
